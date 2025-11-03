feabb885605d4bde56baddba30e0111f54873b9b
Daniel Tilleria <daniel.tilleria@hotmail.com>
2025-11-03 18:28:04 +0100

    d

--- apps/mobile/app/index.tsx
+++ apps/mobile/app/index.tsx
@@ -1,12 +1,19 @@
-import {Text, View} from "react-native";
-import "./global.css";
+import {View, ScrollView} from "react-native";
+import {useRef, useState} from "react";
+
+import Footer from "./components/footer";
+import Header from "./components/header";
+import Hero from "./components/hero";
+import Filter from "./components/filter";
+import ProductCard from "./components/productCard";
+// import "./global.css";
 
 export default function HomePage() {
   const scrollViewRef = useRef<ScrollView>(null);
   const [productY, setProductY] = useState(0);
 
   const scrollToProducts = () => {
-    scrollViewRef.current?.scrollTo({ y: productY, animated: true });
+    scrollViewRef.current?.scrollTo({y: productY, animated: true});
   };
 
   return (
