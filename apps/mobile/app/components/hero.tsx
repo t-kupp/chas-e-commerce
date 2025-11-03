cc73f4549f2a7dfe23c65e9a5238d5a2978911ae
Your Name <you@example.com>
2025-11-03 10:48:57 +0100

    feat: add Hero component to display a "CARDS" text

---
+++ apps/mobile/app/components/hero.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import { View, Text } from "react-native";
+
+export default function Hero() {
+  return (
+    <View>
+      <Text className="text-red-900 m-10">CARDS</Text>
+    </View>
+  );
+}
