3bf300b856d8ca92e1fbe9f2949427bed4e6af8a
Your Name <you@example.com>
2025-11-03 14:37:25 +0100

    feat: add Filter component and integrate it into HomePage layout; create README and devices.json for Expo folder

--- apps/mobile/app/index.tsx
+++ apps/mobile/app/index.tsx
@@ -4,6 +4,7 @@ import Footer from "./components/footer";
 import Header from "./components/header";
 import Hero from "./components/hero";
 import ProductCard from "./components/productCard";
+import Filter from "./components/filter";
 
 export default function HomePage() {
   const scrollViewRef = useRef<ScrollView>(null);
@@ -18,6 +19,7 @@ export default function HomePage() {
       <Header />
       <ScrollView ref={scrollViewRef} className="flex-1">
         <Hero onShopNowPress={scrollToProducts} />
+        <Filter />
         <View onLayout={(event) => setProductY(event.nativeEvent.layout.y)}>
           <ProductCard />
         </View>
