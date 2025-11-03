18ccb52fe88b975d477875937b20c059918ba817
Your Name <you@example.com>
2025-11-03 12:08:30 +0100

    feat: update HomePage layout to use ScrollView for better scrolling experience

--- apps/mobile/app/index.tsx
+++ apps/mobile/app/index.tsx
@@ -1,16 +1,18 @@
 import React from "react";
-import {View} from "react-native";
+import { View, ScrollView } from "react-native";
 import Footer from "./components/footer";
 import Header from "./components/header";
 import Hero from "./components/hero";
+import ProductCard from "./components/productCard";
 
 export default function HomePage() {
   return (
     <View className="flex-1">
-      <View className="flex-1">
+      <ScrollView className="flex-1">
         <Header />
         <Hero />
-      </View>
+        <ProductCard />
+      </ScrollView>
       <Footer />
     </View>
   );
