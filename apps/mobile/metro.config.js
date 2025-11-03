ca635aeebf30ef2f84d6f3a645138e748e939e28
Zarha <zarhaselene@gmail.com>
2025-11-03 18:27:31 +0100

    mobil fix

--- apps/mobile/metro.config.js
+++ apps/mobile/metro.config.js
@@ -1,6 +1,27 @@
-const {getDefaultConfig} = require("expo/metro-config");
-const {withNativeWind} = require("nativewind/metro");
+const { getDefaultConfig } = require("expo/metro-config");
+const { withNativeWind } = require("nativewind/metro");
+const path = require("path");
 
-const config = getDefaultConfig(__dirname);
+const projectRoot = __dirname;
+const workspaceRoot = path.resolve(projectRoot, "../..");
 
-module.exports = withNativeWind(config, {input: "./app/global.css"});
+const config = getDefaultConfig(projectRoot);
+
+// Configure monorepo support
+config.watchFolders = [workspaceRoot];
+
+// Exclude backend and other non-mobile directories
+config.resolver.blockList = [
+  /apps\/backend\/.*/,
+  /apps\/web\/.*/,
+  /apps\/docs\/.*/,
+  /\.git\/.*/,
+  /node_modules\/.*\/node_modules\/react-native\/.*/,
+];
+
+config.resolver.nodeModulesPaths = [
+  path.resolve(projectRoot, "node_modules"),
+  path.resolve(workspaceRoot, "node_modules"),
+];
+
+module.exports = withNativeWind(config, { input: "./app/global.css" });
