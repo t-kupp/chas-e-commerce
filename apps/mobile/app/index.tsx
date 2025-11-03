401dfffa5762d01b0c571f7795e71845892ab336
Daniel Tilleria <daniel.tilleria@hotmail.com>
2025-11-03 11:59:21 +0100

    fix cards

--- apps/mobile/app/index.tsx
+++ apps/mobile/app/index.tsx
@@ -2,12 +2,13 @@ import React from "react";
 import {View} from "react-native";
 import Footer from "./components/footer";
 import Header from "./components/header";
+import ProductCard from "./components/productCard";
 
 export default function HomePage() {
   return (
     <View>
       <Header />
-
+      <ProductCard />
       <Footer />
     </View>
   );
